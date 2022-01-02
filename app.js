e42831e2a03ecb91d27296286632415e840aef0a
Himanshu <himanshu_196018@saitm.org>
2022-01-02 17:34:16 +0530

    design complete

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ const reportClose = document.getElementById('close');
 
 reportClose.addEventListener('click' , () => {
     document.getElementById('todays-report').style.display = 'none';
-})
+});
+
+const disTogg = document.getElementById('disc');
+
+disTogg.addEventListener('click', () => {
+    var toggDis = document.getElementById('togg');
+    toggDis.style.display == 'none' ? toggDis.style.display = 'block': toggDis.style.display = 'none';
+});

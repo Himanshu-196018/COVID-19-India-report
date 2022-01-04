3a3dd4cb2379fc26f8db79f1f6d62425f08d49a9
Himanshu <himanshu_196018@saitm.org>
2022-01-04 15:42:28 +0530

    edited state cases value

--- Getapi.js
+++ Getapi.js
@@ -45,11 +45,22 @@ const act_1 = 24;
 const cur_1 = 7590;
 const dea_1 = 129;
 
+document.querySelector('#con-1').innerHTML = con_1;
+document.querySelector('#act-1').innerHTML = act_1;
+document.querySelector('#Cur-1').innerHTML = cur_1;
+document.querySelector('#Dea-1').innerHTML = dea_1;
+
 //2.
 const con_2 = 2077486;
 const act_2 = 1260;
 const cur_2 = 2061729;
 const dea_2 = 14497;
+
+
+document.querySelector('#con-2').innerHTML = con_2;
+document.querySelector('#act-2').innerHTML = act_2;
+document.querySelector('#Cur-2').innerHTML = cur_2;
+document.querySelector('#Dea-2').innerHTML = dea_2;
 
 //3.
 const con_3 = 55343;
@@ -57,11 +68,21 @@ const act_3 = 16;
 const cur_3 = 55045;
 const dea_3 = 282;
 
+document.querySelector('#con-3').innerHTML = con_3;
+document.querySelector('#act-3').innerHTML = act_3;
+document.querySelector('#Cur-3').innerHTML = cur_3;
+document.querySelector('#Dea-3').innerHTML = dea_3;
+
 //4.
 const con_4 = 621071;
 const act_4 = 2321;
 const cur_4 = 612585;
 const dea_4 = 6165;
+
+document.querySelector('#con-4').innerHTML = con_4;
+document.querySelector('#act-4').innerHTML = act_4;
+document.querySelector('#Cur-4').innerHTML = cur_4;
+document.querySelector('#Dea-4').innerHTML = dea_4;
 
 //5.
 const con_5 = 727529;
@@ -69,11 +90,21 @@ const act_5 = 1075;
 const cur_5 = 714358;
 const dea_5 = 12096;
 
+document.querySelector('#con-5').innerHTML = con_5;
+document.querySelector('#act-5').innerHTML = act_5;
+document.querySelector('#Cur-5').innerHTML = cur_5;
+document.querySelector('#Dea-5').innerHTML = dea_5;
+
 //6.
 const con_6 = 66061;
 const act_6 = 321;
 const cur_6 = 64661;
 const dea_6 = 1079;
+
+document.querySelector('#con-6').innerHTML = con_6;
+document.querySelector('#act-6').innerHTML = act_6;
+document.querySelector('#Cur-6').innerHTML = cur_6;
+document.querySelector('#Dea-6').innerHTML = dea_6;
 
 //7.
 const con_7 = 1008756;
@@ -81,11 +112,21 @@ const act_7 = 1273;
 const cur_7 = 993882;
 const dea_7 = 13601;
 
+document.querySelector('#con-7').innerHTML = con_7;
+document.querySelector('#act-7').innerHTML = act_7;
+document.querySelector('#Cur-7').innerHTML = cur_7;
+document.querySelector('#Dea-7').innerHTML = dea_7;
+
 //8.
 const con_8 = 10700;
 const act_8 = 9;
 const cur_8 = 10687;
 const dea_8 = 4;
+
+document.querySelector('#con-8').innerHTML = con_8;
+document.querySelector('#act-8').innerHTML = act_8;
+document.querySelector('#Cur-8').innerHTML = cur_8;
+document.querySelector('#Dea-8').innerHTML = dea_8;
 
 //9.
 const con_9 = 1454121;
@@ -93,11 +134,21 @@ const act_9 = 8397;
 const cur_9 = 1420615;
 const dea_9 = 25109;
 
+document.querySelector('#con-9').innerHTML = con_9;
+document.querySelector('#act-9').innerHTML = act_9;
+document.querySelector('#Cur-9').innerHTML = cur_9;
+document.querySelector('#Dea-9').innerHTML = dea_9;
+
 //10.
 const con_10 = 181570;
 const act_10 = 1671;
 const cur_10 = 176376;
 const dea_10 = 3523;
+
+document.querySelector('#con-10').innerHTML = con_10;
+document.querySelector('#act-10').innerHTML = act_10;
+document.querySelector('#Cur-10').innerHTML = cur_10;
+document.querySelector('#Dea-10').innerHTML = dea_10;
 
 //11.
 const con_11 = 833769;
@@ -105,11 +156,21 @@ const act_11 = 4753;
 const cur_11 = 818896;
 const dea_11 = 10120;
 
+document.querySelector('#con-11').innerHTML = con_11;
+document.querySelector('#act-11').innerHTML = act_11;
+document.querySelector('#Cur-11').innerHTML = cur_11;
+document.querySelector('#Dea-11').innerHTML = dea_11;
+
 //12.
 const con_12 = 774917;
 const act_12 = 2423;
 const cur_12 = 762430;
 const dea_12 = 10064;
+
+document.querySelector('#con-12').innerHTML = con_12;
+document.querySelector('#act-12').innerHTML = act_12;
+document.querySelector('#Cur-12').innerHTML = cur_12;
+document.querySelector('#Dea-12').innerHTML = dea_12;
 
 //13.
 const con_13 = 229016;
@@ -117,11 +178,21 @@ const act_13 = 526;
 const cur_13 = 224612;
 const dea_13 = 3878;
 
+document.querySelector('#con-13').innerHTML = con_13;
+document.querySelector('#act-13').innerHTML = act_13;
+document.querySelector('#Cur-13').innerHTML = cur_13;
+document.querySelector('#Dea-13').innerHTML = dea_13;
+
 //14.
 const con_14 = 341624;
 const act_14 = 1429;
 const cur_14 = 335665;
 const dea_14 = 4530;
+
+document.querySelector('#con-14').innerHTML = con_14;
+document.querySelector('#act-14').innerHTML = act_14;
+document.querySelector('#Cur-14').innerHTML = cur_14;
+document.querySelector('#Dea-14').innerHTML = dea_14;
 
 //15.
 const con_15 = 353785;
@@ -129,11 +200,21 @@ const act_15 = 3824;
 const cur_15 = 344815;
 const dea_15 = 5146;
 
+document.querySelector('#con-15').innerHTML = con_15;
+document.querySelector('#act-15').innerHTML = act_15;
+document.querySelector('#Cur-15').innerHTML = cur_15;
+document.querySelector('#Dea-15').innerHTML = dea_15;
+
 //16.
 const con_16 = 3009557;
 const act_16 = 10321;
 const cur_16 = 2960890;
 const dea_16 = 38346;
+
+document.querySelector('#con-16').innerHTML = con_16;
+document.querySelector('#act-16').innerHTML = act_16;
+document.querySelector('#Cur-16').innerHTML = cur_16;
+document.querySelector('#Dea-16').innerHTML = dea_16;
 
 //17.
 const con_17 = 5252414;
@@ -141,11 +222,21 @@ const act_17 = 19714;
 const cur_17 = 5184587;
 const dea_17 = 48113;
 
+document.querySelector('#con-17').innerHTML = con_17;
+document.querySelector('#act-17').innerHTML = act_17;
+document.querySelector('#Cur-17').innerHTML = cur_17;
+document.querySelector('#Dea-17').innerHTML = dea_17;
+
 //18.
 const con_18 = 22224;
 const act_18 = 196;
 const cur_18 = 21808;
 const dea_18 = 220;
+
+document.querySelector('#con-18').innerHTML = con_18;
+document.querySelector('#act-18').innerHTML = act_18;
+document.querySelector('#Cur-18').innerHTML = cur_18;
+document.querySelector('#Dea-18').innerHTML = dea_18;
 
 //19.
 const con_19 = 10417;
@@ -153,11 +244,21 @@ const act_19 = 1;
 const cur_19 = 10365;
 const dea_19 = 51;
 
+document.querySelector('#con-19').innerHTML = con_19;
+document.querySelector('#act-19').innerHTML = act_19;
+document.querySelector('#Cur-19').innerHTML = cur_19;
+document.querySelector('#Dea-19').innerHTML = dea_19;
+
 //20.
 const con_20 = 794240;
 const act_20 = 608;
 const cur_20 = 783099;
 const dea_20 = 10533;
+
+document.querySelector('#con-20').innerHTML = con_20;
+document.querySelector('#act-20').innerHTML = act_20;
+document.querySelector('#Cur-20').innerHTML = cur_20;
+document.querySelector('#Dea-20').innerHTML = dea_20;
 
 //21.
 const con_21 = 799868;
@@ -165,11 +266,21 @@ const act_21 = 45716;
 const cur_21 = 612610;
 const dea_21 = 141542;
 
+document.querySelector('#con-21').innerHTML = con_21;
+document.querySelector('#act-21').innerHTML = act_21;
+document.querySelector('#Cur-21').innerHTML = cur_21;
+document.querySelector('#Dea-21').innerHTML = dea_21;
+
 //22.
 const con_22 = 125823;
 const act_22 = 215;
 const cur_22 = 123605;
 const dea_22 = 2003;
+
+document.querySelector('#con-22').innerHTML = con_22;
+document.querySelector('#act-22').innerHTML = act_22;
+document.querySelector('#Cur-22').innerHTML = cur_22;
+document.querySelector('#Dea-22').innerHTML = dea_22;
 
 //23.
 const con_23 = 84847;
@@ -177,11 +288,21 @@ const act_23 = 75;
 const cur_23 = 83288;
 const dea_23 = 1484;
 
+document.querySelector('#con-23').innerHTML = con_23;
+document.querySelector('#act-23').innerHTML = act_23;
+document.querySelector('#Cur-23').innerHTML = cur_23;
+document.querySelector('#Dea-23').innerHTML = dea_23;
+
 //24.
 const con_24 = 131901;
 const act_24 = 1732;
 const cur_24 = 129623;
 const dea_24 = 546;
+
+document.querySelector('#con-24').innerHTML = con_24;
+document.querySelector('#act-24').innerHTML = act_24;
+document.querySelector('#Cur-24').innerHTML = cur_24;
+document.querySelector('#Dea-24').innerHTML = dea_24;
 
 //25.
 const con_25 = 32202;
@@ -189,11 +310,21 @@ const act_25 = 60;
 const cur_25 = 31440;
 const dea_25 = 702;
 
+document.querySelector('#con-25').innerHTML = con_25;
+document.querySelector('#act-25').innerHTML = act_25;
+document.querySelector('#Cur-25').innerHTML = cur_25;
+document.querySelector('#Dea-25').innerHTML = dea_25;
+
 //26.
 const con_26 = 1055556;
 const act_26 = 1984;
 const cur_26 = 1045109;
 const dea_26 = 8463;
+
+document.querySelector('#con-26').innerHTML = con_26;
+document.querySelector('#act-26').innerHTML = act_26;
+document.querySelector('#Cur-26').innerHTML = cur_26;
+document.querySelector('#Dea-26').innerHTML = dea_26;
 
 //27.
 const con_27 = 129527;
@@ -201,11 +332,21 @@ const act_27 = 150;
 const cur_27 = 127496;
 const dea_27 = 1881;
 
+document.querySelector('#con-27').innerHTML = con_27;
+document.querySelector('#act-27').innerHTML = act_27;
+document.querySelector('#Cur-27').innerHTML = cur_27;
+document.querySelector('#Dea-27').innerHTML = dea_27;
+
 //28.
 const con_28 = 605409;
 const act_28 = 1269;
 const cur_28 = 587492;
 const dea_28 = 16648;
+
+document.querySelector('#con-28').innerHTML = con_28;
+document.querySelector('#act-28').innerHTML = act_28;
+document.querySelector('#Cur-28').innerHTML = cur_28;
+document.querySelector('#Dea-28').innerHTML = dea_28;
 
 //29.
 const con_29 = 956883;
@@ -213,11 +354,21 @@ const act_29 = 1572;
 const cur_29 = 946347;
 const dea_29 = 8964;
 
+document.querySelector('#con-29').innerHTML = con_29;
+document.querySelector('#act-29').innerHTML = act_29;
+document.querySelector('#Cur-29').innerHTML = cur_29;
+document.querySelector('#Dea-29').innerHTML = dea_29;
+
 //30.
 const con_30 = 32517;
 const act_30 = 58;
 const cur_30 = 32050;
 const dea_30 = 409;
+
+document.querySelector('#con-30').innerHTML = con_30;
+document.querySelector('#act-30').innerHTML = act_30;
+document.querySelector('#Cur-30').innerHTML = cur_30;
+document.querySelector('#Dea-30').innerHTML = dea_30;
 
 //31.
 const con_31 = 2751128;
@@ -225,11 +376,21 @@ const act_31 = 9304;
 const cur_31 = 2705034;
 const dea_31 = 36790;
 
+document.querySelector('#con-31').innerHTML = con_31;
+document.querySelector('#act-31').innerHTML = act_31;
+document.querySelector('#Cur-31').innerHTML = cur_31;
+document.querySelector('#Dea-31').innerHTML = dea_31;
+
 //32.
 const con_32 = 682489;
 const act_32 = 3779;
 const cur_32 = 674680;
 const dea_32 = 4030;
+
+document.querySelector('#con-32').innerHTML = con_32;
+document.querySelector('#act-32').innerHTML = act_32;
+document.querySelector('#Cur-32').innerHTML = cur_32;
+document.querySelector('#Dea-32').innerHTML = dea_32;
 
 //33.
 const con_33 = 85120;
@@ -237,11 +398,21 @@ const act_33 = 119;
 const cur_33 = 84172;
 const dea_33 = 829;
 
+document.querySelector('#con-33').innerHTML = con_33;
+document.querySelector('#act-33').innerHTML = act_33;
+document.querySelector('#Cur-33').innerHTML = cur_33;
+document.querySelector('#Dea-33').innerHTML = dea_33;
+
 //34.
 const con_34 = 345464;
 const act_34 = 506;
 const cur_34 = 337539;
 const dea_34 = 7419;
+
+document.querySelector('#con-34').innerHTML = con_34;
+document.querySelector('#act-34').innerHTML = act_34;
+document.querySelector('#Cur-34').innerHTML = cur_34;
+document.querySelector('#Dea-34').innerHTML = dea_34;
 
 //35.
 const con_35 = 1712537;
@@ -249,9 +420,19 @@ const act_35 = 1725;
 const cur_35 = 1687896;
 const dea_35 = 22916;
 
+document.querySelector('#con-35').innerHTML = con_35;
+document.querySelector('#act-35').innerHTML = act_35;
+document.querySelector('#Cur-35').innerHTML = cur_35;
+document.querySelector('#Dea-35').innerHTML = dea_35;
+
 //36.
 const con_36 = 1649150;
 const act_36 = 17038;
 const cur_36 = 1612331;
 const dea_36 = 19781;
+
+document.querySelector('#con-36').innerHTML = con_36;
+document.querySelector('#act-36').innerHTML = act_36;
+document.querySelector('#Cur-36').innerHTML = cur_36;
+document.querySelector('#Dea-36').innerHTML = dea_36;
 
